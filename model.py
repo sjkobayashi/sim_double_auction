629d7377b0cfd9803ac40a2142f08f4a3ed2503a
Shunto <kobayashunto@gmail.com>
2018-04-01 10:38:59 -0700

    Skelton of Cliff. Before modifying the CDAmodel.

--- model.py
+++ model.py
@@ -6,6 +6,48 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 
+# ------------------------------ scheduler ------------------------------
+
+
+class CliffScheduler:
+    def __init__(self, model):
+        self.model = model
+        self.steps = 0
+        self.time = 0
+        self.buyers = []
+        self.sellers = []
+
+    def add_buyer(self, buyer):
+        self.buyers.append(buyer)
+
+    def add_seller(self, seller):
+        self.sellers.append(seller)
+
+    def remove_buyer(self, buyer):
+        while buyer in self.buyers:
+            self.buyers.remove(buyer)
+
+    def remove_seller(self, seller):
+        while seller in self.sellers:
+            self.sellers.append(seller)
+
+    def get_agent_count(self):
+        return len(self.buyers) + len(self.sellers)
+
+    def step(self):
+        proposer = random.choice(self.buyers + self.sellers)
+        proposer.step()
+        if "last guy was seller":
+            active_buyers = [buyer for buyer in self.buyers if buyer.active]
+            willing_buyers = [buyer for buyer in active_buyers if buyer.value >= "ask"]
+        else "last guy was buyer":
+            active_sellers = [buyer for seller in self.sellers if seller.active]
+            willing_sellers = [seller for seller in self.sellers if seller.value <= "bid"]
+        respoder.step()
+
+
+# ------------------------------ Data functions ------------------------------
+
 
 def get_bidder_id(model):
     try:
@@ -30,17 +72,28 @@ def compute_efficiency(model):
     ))
     return actual_surplus / total_surplus
 
+# ------------------------------ Traders ------------------------------
 
-class ZeroIntelligence(Agent):
-    """Zero-Intelligence strategy from Gode and Sunder (1993)"""
+
+class Trader(Agent):
     def __init__(self, unique_id, model, role, value, q):
         super().__init__(unique_id, model)
         self.role = role  # buyer or seller
         self.value = value  # value or cost
 
+        if role == "seller":
+            self.good = q  # good owned
+            self.right = 0
+            self.strategy = self._sell_strategy
+        elif role == "buyer":
+            self.good = 0
+            self.right = q  # right to buy
+            self.strategy = self._buy_strategy
+        else:
+            raise ValueError("Role must be 'seller' or 'buyer'.")
+
         self.surplus = 0  # profit
-        self.right = q  # right to buy
-        self.good = q  # good owned
+        self.active = True
 
     def bid(self, price):
         # Submit a bid to the CDA.
@@ -54,34 +107,58 @@ class ZeroIntelligence(Agent):
         self.good += 1
         self.right -= 1
         self.surplus += self.value - price
+        if self.right == 0:
+            # if buyer's rights become zero, it deactivates.
+            self.active = False
 
     def sell(self, price):
         self.good -= 1
         self.right += 1
         self.surplus += price - self.value
+        if self.good == 0:
+            # if seller's units become zero, it deactivates.
+            self.active = False
+
+    def _sell_strategy(self):
+        pass
+
+    def _buy_strategy(self):
+        pass
 
     def step(self):
-        if self.role == "buyer":
-            price = random.randint(1, self.value)
-            if (self.right > 0):
-                self.bid(price)
-        else:
-            price = random.randint(self.value, 200)
-            if (self.good > 0):
-                self.ask(price)
-        self.model.datacollector.collect(self.model)
-        self.model.next_tick()
+        if self.active:
+            self.strategy()
+            self.model.datacollector.collect(self.model)
+            self.model.next_tick()
+
+
+class ZI(Trader):
+    """Zero-Intelligence strategy from Gode and Sunder (1993)"""
+    def _sell_strategy(self):
+        price = random.randint(self.value, 200)
+        self.ask(price)
+
+    def _buy_strategy(self):
+        price = random.randint(1, self.value)
+        self.bid(price)
+
+
+class ZIP(Trader):
+    """Zero-Intelligence-Plus strategy from Cliff (1997)"""
+    def _sell_strategy(self):
+        a
+
+# ------------------------------ CDA ------------------------------
 
 
 class CDAmodel(Model):
     """Continuous Double Auction model with some number of agents."""
     def __init__(self, supply, demand):
         self.period = 1
-        self.tick = 1
         self.supply = supply
         self.demand = demand
-        self.num_buyers = demand.num_agents
         self.num_sellers = supply.num_agents
+        self.num_buyers = demand.num_agents
         self.initialize_spread()
         self.market_price = None
         # How agents are activated at each step
@@ -89,13 +166,12 @@ class CDAmodel(Model):
         # Create agents
         for i, value in enumerate(demand.price_schedule):
             self.schedule.add(
-                ZeroIntelligence(i, self, "buyer", value, demand.q_per_agent)
+                ZI(i, self, "buyer", value, demand.q_per_agent)
             )
         for i, cost in enumerate(supply.price_schedule):
+            j = self.num_buyers + i
             self.schedule.add(
-                ZeroIntelligence(
-                    self.num_buyers + i, self, "seller", cost, supply.q_per_agent
-                )
+                ZI(j, self, "seller", cost, supply.q_per_agent)
             )
 
         # Collecting data
@@ -106,7 +182,7 @@ class CDAmodel(Model):
                              "OBer": get_bidder_id,
                              "OA": "outstanding_ask",
                              "OAer": get_asker_id,
-                             "Market Price": "market_price",
+                             "MarketPrice": "market_price",
                              "Traded": "traded"},
             agent_reporters={"Period": lambda x: x.model.period,
                              "Tick": lambda x: x.model.tick,
@@ -154,9 +230,25 @@ class CDAmodel(Model):
         self.tick += 1
 
     def step(self):
+        self.tick = 1
         self.schedule.step()
         self.period += 1
-        self.tick = 1
+
+    def plot_model(self):
+        data = self.datacollector.get_model_vars_dataframe()
+        data = data[data.Traded == 1]
+        f = plt.figure(1)
+        ax = f.add_subplot(111)
+        plt.plot(data.MarketPrice)
+        plt.axhline(y=self.supply.equilibrium_price,
+                    color='black',
+                    linestyle='dashed'
+                    )
+        plt.text(0.8, 0.9, round(compute_efficiency(self), 3), fontsize=20,
+                 transform=ax.transAxes)
+        plt.show()
+
+# ------------------------------ Supply & Demand ------------------------------
 
 
 class Supply:
@@ -216,6 +308,7 @@ class Supply:
         iterable = (p * self.q_per_agent for p in surplus)
         self.theoretical_surplus = sum(np.fromiter(iterable, float))
 
+
 class Demand(Supply):
     def __init__(self, num_in, num_ex, q, p_max, p_eqb, num_per_step):
         self._init_common(num_in, num_ex, q, p_max, p_eqb, num_per_step)
@@ -236,9 +329,10 @@ class Demand(Supply):
         iterable = (p * self.q_per_agent for p in surplus)
         self.theoretical_surplus = sum(np.fromiter(iterable, float))
 
+# ------------------------------ Simulation ------------------------------
+
 
 # equilibrium price is 110
-# equilibrium quantity is 24
 supply = Supply(10, 4, 3, 10, 110, 2)
 demand = Demand(10, 4, 3, 210, 110, 2)
 
@@ -250,3 +344,5 @@ data_model = model.datacollector.get_model_vars_dataframe()
 data_model = data_model[data_model.Traded == 1]
 
 data_agent = model.datacollector.get_agent_vars_dataframe()
+
+model.plot_model()
