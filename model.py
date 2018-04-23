e95f4267495ba2a14c4e4b52544489bd1204152f
Shunto <kobayashunto@gmail.com>
2018-04-22 20:47:59 -0700

    Added memory length to GD agents. Decided not to add logging for now.

--- model.py
+++ model.py
@@ -69,159 +69,9 @@ class GD(Trader):
     def __init__(self, unique_id, model, role, value, q):
         super().__init__(unique_id, model, role, value, q)
         self.eta = 0.001
-        self.highest_ask = 10
-        self.lowest_bid = 0
-
-    def _sell_belief(self, ask):
-        """Compute the seller's belief that an ask will be accepted."""
-        if ask >= self.model.outstanding_ask:
-            return 0
-
-        def num_geq(history, price):
-            return len([d for d in history if d >= price])
-
-        def num_leq(history, price):
-            return len([d for d in history if d <= price])
-
-        history = self.model.history
-        asks = history.get_asks()
-        accepted_asks = history.get_accepted_asks()
-        accepted_bids = history.get_accepted_bids()
-
-        TAG = num_geq(accepted_asks, ask)
-        TBG = num_geq(accepted_bids, ask)
-        RAL = num_leq(asks, ask) - num_leq(accepted_asks, ask)
-
-        if TAG + TBG == 0:
-            return self.eta
-        else:
-            return (TAG + TBG) / (TAG + TBG + RAL)
-
-    def _buy_belief(self, bid):
-        """Compute the buyer's belief that an bid will be accepted."""
-        if bid <= self.model.outstanding_bid:
-            return 0
-
-        def num_geq(history, price):
-            return len([d for d in history if d >= price])
-
-        def num_leq(history, price):
-            return len([d for d in history if d <= price])
-
-        history = self.model.history
-        bids = history.get_bids()
-        accepted_asks = history.get_accepted_asks()
-        accepted_bids = history.get_accepted_bids()
-
-        TBL = num_leq(accepted_bids, bid)
-        TAL = num_leq(accepted_asks, bid)
-        RBG = num_geq(bids, bid) - num_geq(accepted_bids, bid)
-
-        if TBL + TAL == 0:
-            return self.eta
-        else:
-            return (TBL + TAL) / (TBL + TAL + RBG)
-
-    def _sell_surplus_maximizer(self, prices):
-        """Given a list of prices, find the price that maximizes \
-        the seller's expected surplus."""
-        def exp_surplus(price):
-            return (price - self.value) * self._sell_belief(price)
-
-        exp_surpluses = [exp_surplus(price) for price in prices]
-        max_index, max_es = max(enumerate(exp_surpluses), key=lambda p: p[1])
-        return max_index
-
-    def _buy_surplus_maximizer(self, prices):
-        """Given a list of prices, find the price that maximizes \
-        the buyer's expected surplus."""
-        def exp_surplus(price):
-            return (self.value - price) * self._buy_belief(price)
-
-        exp_surpluses = [exp_surplus(price) for price in prices]
-        max_index, max_es = max(enumerate(exp_surpluses), key=lambda p: p[1])
-        return max_index
-
-    def _sell_strategy(self):
-        """Randomize the expected surplus maximizing price \
-        and submit it an ask."""
-        if self.value >= self.model.outstanding_ask:
-            self.do_nothing()
-            return
-
-        if self.model.num_traded == 0:
-            # If no trade has occurred, be a smarter ZI.
-            a = self.value
-            b = min(self.model.outstanding_ask, self.highest_ask)
-        else:
-            prices = self.model.history.get_prices()
-            prices.append(self.lowest_bid)
-            prices.append(self.highest_ask)
-            prices.sort()
-            max_index = self._sell_surplus_maximizer(prices)
-            max_price = prices[max_index]
-
-            if max_index == 0:
-                smallest_gap = prices[max_index + 1] - max_price
-            elif max_index == len(prices) - 1:
-                smallest_gap = max_price - prices[max_index - 1]
-            else:
-                smallest_gap = min(max_price - prices[max_index - 1],
-                                   prices[max_index + 1] - max_price)
-            # max_price - smallest_gap results in the outstanding ask
-            # if eta is given for ask >= oa
-            a = max(max_price - smallest_gap, self.value)
-            b = min(max_price + smallest_gap, self.model.outstanding_ask)
-
-        planned_ask = np.random.uniform(a, b)
-        self.ask(planned_ask)
-        self.model.unif = (a, b)
-
-    def _buy_strategy(self):
-        """Randomize the expected surplus maximizing price \
-        and submit it a bid."""
-
-        if self.value <= self.model.outstanding_bid:
-            self.do_nothing()
-            return
-
-        if self.model.num_traded == 0:
-            # If no trade has occurred, be a smarter ZI.
-            a = max(0, self.model.outstanding_bid)
-            b = self.value
-        else:
-            prices = self.model.history.get_prices()
-            prices.append(self.lowest_bid)
-            prices.append(self.highest_ask)
-            prices.sort()
-            max_index = self._buy_surplus_maximizer(prices)
-            max_price = prices[max_index]
-
-            if max_index == 0:
-                smallest_gap = prices[max_index + 1] - max_price
-            elif max_index == len(prices) - 1:
-                smallest_gap = max_price - prices[max_index - 1]
-            else:
-                smallest_gap = min(max_price - prices[max_index - 1],
-                                   prices[max_index + 1] - max_price)
-            # max_price - smallest_gap results in the outstanding ask
-            # if eta is given for ask >= oa
-            a = max(max_price - smallest_gap, self.model.outstanding_bid)
-            b = min(max_price + smallest_gap, self.value)
-
-        planned_bid = np.random.uniform(a, b)
-        self.bid(planned_bid)
-        self.model.unif = (a, b)
-
-
-
-class GDtest(Trader):
-    """Gjerstad-Dickhaut strategy from Gjerstad and Dickhaut (1998)"""
-    def __init__(self, unique_id, model, role, value, q):
-        super().__init__(unique_id, model, role, value, q)
-        self.eta = 0.001
         self.highest_ask = 10.0
         self.lowest_bid = 0.0
+        self.mem_length = 3
 
     def _sell_belief(self, ask):
         """Compute the seller's belief that an ask will be accepted."""
@@ -235,9 +85,9 @@ class GDtest(Trader):
             return len([d for d in history if d <= price])
 
         history = self.model.history
-        asks = history.get_asks()
-        accepted_asks = history.get_accepted_asks()
-        accepted_bids = history.get_accepted_bids()
+        asks = history.get_asks(length=self.mem_length)
+        accepted_asks = history.get_accepted_asks(length=self.mem_length)
+        accepted_bids = history.get_accepted_bids(length=self.mem_length)
 
         TAG = num_geq(accepted_asks, ask)
         TBG = num_geq(accepted_bids, ask)
@@ -260,9 +110,9 @@ class GDtest(Trader):
             return len([d for d in history if d <= price])
 
         history = self.model.history
-        bids = history.get_bids()
-        accepted_asks = history.get_accepted_asks()
-        accepted_bids = history.get_accepted_bids()
+        bids = history.get_bids(length=self.mem_length)
+        accepted_asks = history.get_accepted_asks(length=self.mem_length)
+        accepted_bids = history.get_accepted_bids(length=self.mem_length)
 
         TBL = num_leq(accepted_bids, bid)
         TAL = num_leq(accepted_asks, bid)
@@ -315,7 +165,7 @@ class GDtest(Trader):
             a = self.value
             b = min(self.model.outstanding_ask, self.highest_ask)
         else:
-            prices = self.model.history.get_prices()
+            prices = self.model.history.get_prices(length=self.mem_length)
 
             if self.lowest_bid not in prices:
                 prices.append(self.lowest_bid)
@@ -362,7 +212,7 @@ class GDtest(Trader):
             a = max(0, self.model.outstanding_bid)
             b = self.value
         else:
-            prices = self.model.history.get_prices()
+            prices = self.model.history.get_prices(length=self.mem_length)
 
             if self.lowest_bid not in prices:
                 prices.append(self.lowest_bid)
@@ -397,7 +247,7 @@ class GDtest(Trader):
 
 # ------------------------------ CDA ------------------------------
 
-# exclude outstanding spread?
+
 class Order_history:
     def __init__(self):
         self.orders = list()
@@ -636,6 +486,14 @@ class CDAmodel(Model):
         plt.text(0.8, 0.9, round(compute_efficiency(self), 3), fontsize=20,
                  transform=ax.transAxes)
         plt.show()
+
+# ------------------------------ Logger ----------------------------------
+
+# logging.basicconfig(level="debug", filename='simulation.log',
+#                     filemode='w')
+# logger = logging.getlogger("model")
+# logger.debug("starting a simulation.")
+
 
 # ------------------------------ Simulation ------------------------------
 
